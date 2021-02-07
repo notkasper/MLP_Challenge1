c51ad37e555035894d0aac80baefd50cf7f49830
kasper karelse <kasperkarelse98@gmail.com>
2021-02-07 17:08:49 +0100

    simple starter GAN

---
+++ model.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+
+
+class Discriminator(nn.Module):
+    def __init__(self, img_dim):
+        super().__init__()
+        self.disc = nn.Sequential(
+            nn.Linear(img_dim, 128),
+            nn.LeakyReLU(0.1),
+            nn.Linear(128, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return self.disc(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, img_dim):
+        super().__init__()
+        self.gen = nn.Sequential(
+            nn.Linear(z_dim, 256),
+            nn.LeakyReLU(0.1),
+            nn.Linear(256, img_dim),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        return self.gen(x)
